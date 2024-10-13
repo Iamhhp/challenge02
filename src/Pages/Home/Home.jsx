3946d7a081274a237cf47829f6ac258d747e6405
Iamhhp <Iamhhp2020@gmail.com>
2024-10-13 23:55:37 +0330

    adding cause-success

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,6 +1,17 @@
+import './Home.css';
+import CauseSuccess from '../../Components/CauseSuccess/CauseSuccess';
 import Hero from '../../Components/Hero/Hero';
 import ServiceUs from '../../Components/ServiveUs/ServiceUs';
 import University from '../../Components/University/University';
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+// import required modules
+import { Navigation, Pagination } from 'swiper/modules';
 
 function Home() {
   return (
@@ -8,6 +19,23 @@ function Home() {
       <Hero />
       <University />
       <ServiceUs />
+      <Swiper pagination navigation modules={[Pagination, Navigation]} className='my-swiper'>
+        <SwiperSlide>
+          <CauseSuccess />
+        </SwiperSlide>
+
+        <SwiperSlide>
+          <CauseSuccess />
+        </SwiperSlide>
+
+        <SwiperSlide>
+          <CauseSuccess />
+        </SwiperSlide>
+
+        <SwiperSlide>
+          <CauseSuccess />
+        </SwiperSlide>
+      </Swiper>
     </>
   );
 }
